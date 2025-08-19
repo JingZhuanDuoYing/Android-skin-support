1ef2c791a95ea992943cd65a590a66549a7099a5
donglua <927878309@qq.com>
2025-08-19 14:23:27 +0800

    优化代码，增强代码可读性和安全性，改进异常处理

--- androidx/skin-support-appcompat/src/main/java/skin/support/widget/SkinCompatTextView.java
+++ androidx/skin-support-appcompat/src/main/java/skin/support/widget/SkinCompatTextView.java
@@ -1,81 +1,138 @@
 package skin.support.widget;
 
 import android.content.Context;
-import androidx.annotation.DrawableRes;
-import androidx.appcompat.widget.AppCompatTextView;
 import android.util.AttributeSet;
+import android.util.Log;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
 
 /**
- * Created by ximsfei on 2017/1/10.
+ * 扩展 {@link AppCompatTextView}，支持动态换肤的文本视图。
+ * 通过 {@link SkinCompatBackgroundHelper} 处理背景换肤，
+ * 通过 {@link SkinCompatTextHelper} 处理文本和 drawable 换肤。
+ *
+ * @author ximsfei
+ * @since 2017/1/10
  */
-
 public class SkinCompatTextView extends AppCompatTextView implements SkinCompatSupportable {
-    private SkinCompatTextHelper mTextHelper;
-    private SkinCompatBackgroundHelper mBackgroundTintHelper;
+    private static final String TAG = "SkinCompatTextView";
+    private final SkinCompatBackgroundHelper mBackgroundTintHelper;
+    private final SkinCompatTextHelper mTextHelper;
 
-    public SkinCompatTextView(Context context) {
+    public SkinCompatTextView(@NonNull Context context) {
         this(context, null);
     }
 
-    public SkinCompatTextView(Context context, AttributeSet attrs) {
+    public SkinCompatTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, android.R.attr.textViewStyle);
     }
 
-    public SkinCompatTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public SkinCompatTextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mBackgroundTintHelper = new SkinCompatBackgroundHelper(this);
-        mBackgroundTintHelper.loadFromAttributes(attrs, defStyleAttr);
         mTextHelper = SkinCompatTextHelper.create(this);
-        mTextHelper.loadFromAttributes(attrs, defStyleAttr);
-    }
-
-    @Override
-    public void setBackgroundResource(@DrawableRes int resId) {
-        super.setBackgroundResource(resId);
-        if (mBackgroundTintHelper != null) {
-            mBackgroundTintHelper.onSetBackgroundResource(resId);
+        try {
+            mBackgroundTintHelper.loadFromAttributes(attrs, defStyleAttr);
+            mTextHelper.loadFromAttributes(attrs, defStyleAttr);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to load attributes for SkinCompatTextView", e);
         }
     }
 
+    /**
+     * 设置背景资源，并通知换肤助手更新背景。
+     *
+     * @param resId 背景资源 ID
+     */
+    @Override
+    public void setBackgroundResource(@DrawableRes int resId) {
+        super.setBackgroundResource(resId);
+        try {
+            mBackgroundTintHelper.onSetBackgroundResource(resId);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set background resource: " + resId, e);
+        }
+    }
+
+    /**
+     * 设置文本外观样式。
+     *
+     * @param resId 文本外观资源 ID
+     */
     @Override
     public void setTextAppearance(int resId) {
         setTextAppearance(getContext(), resId);
     }
 
+    /**
+     * 设置文本外观样式，并通知换肤助手更新。
+     *
+     * @param context 上下文
+     * @param resId   文本外观资源 ID
+     */
     @Override
-    public void setTextAppearance(Context context, int resId) {
+    public void setTextAppearance(@NonNull Context context, int resId) {
         super.setTextAppearance(context, resId);
-        if (mTextHelper != null) {
+        try {
             mTextHelper.onSetTextAppearance(context, resId);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set text appearance: " + resId, e);
         }
     }
 
+    /**
+     * 设置相对方向的复合 drawable，并通知换肤助手更新。
+     *
+     * @param start  开始方向的 drawable 资源 ID
+     * @param top    顶部 drawable 资源 ID
+     * @param end    结束方向的 drawable 资源 ID
+     * @param bottom 底部 drawable 资源 ID
+     */
     @Override
     public void setCompoundDrawablesRelativeWithIntrinsicBounds(
             @DrawableRes int start, @DrawableRes int top, @DrawableRes int end, @DrawableRes int bottom) {
         super.setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
-        if (mTextHelper != null) {
+        try {
             mTextHelper.onSetCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set compound drawables relative: start=" + start + ", top=" + top +
+                    ", end=" + end + ", bottom=" + bottom, e);
         }
     }
 
+    /**
+     * 设置复合 drawable，并通知换肤助手更新。
+     *
+     * @param left  左侧 drawable 资源 ID
+     * @param top   顶部 drawable 资源 ID
+     * @param right 右侧 drawable 资源 ID
+     * @param bottom 底部 drawable 资源 ID
+     */
     @Override
     public void setCompoundDrawablesWithIntrinsicBounds(
             @DrawableRes int left, @DrawableRes int top, @DrawableRes int right, @DrawableRes int bottom) {
         super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
-        if (mTextHelper != null) {
+        try {
             mTextHelper.onSetCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set compound drawables: left=" + left + ", top=" + top +
+                    ", right=" + right + ", bottom=" + bottom, e);
         }
     }
 
+    /**
+     * 应用当前皮肤，更新背景和文本样式。
+     */
     @Override
     public void applySkin() {
-        if (mBackgroundTintHelper != null) {
+        try {
             mBackgroundTintHelper.applySkin();
-        }
-        if (mTextHelper != null) {
             mTextHelper.applySkin();
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to apply skin", e);
         }
     }
-
 }
--- androidx/skin-support-appcompat/src/main/java/skin/support/widget/SkinCompatToolbar.java
+++ androidx/skin-support-appcompat/src/main/java/skin/support/widget/SkinCompatToolbar.java
@@ -2,10 +2,13 @@ package skin.support.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+
 import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
-import android.util.AttributeSet;
 
 import skin.support.content.res.SkinCompatResources;
 import skin.support.content.res.SkinCompatVectorResources;
@@ -15,27 +18,35 @@ import skin.support.R;
 import static skin.support.widget.SkinCompatHelper.INVALID_ID;
 
 /**
- * Created by ximsfei on 17-1-12.
+ * 扩展 {@link Toolbar}，支持动态换肤的工具栏。
+ * 支持背景、标题颜色、副标题颜色和导航图标的换肤功能。
+ *
+ * @author ximsfei
+ * @since 2017/1/12
  */
-
 public class SkinCompatToolbar extends Toolbar implements SkinCompatSupportable {
+    private static final String TAG = "SkinCompatToolbar";
     private int mTitleTextColorResId = INVALID_ID;
     private int mSubtitleTextColorResId = INVALID_ID;
     private int mNavigationIconResId = INVALID_ID;
-    private SkinCompatBackgroundHelper mBackgroundTintHelper;
+    private final SkinCompatBackgroundHelper mBackgroundTintHelper;
 
-    public SkinCompatToolbar(Context context) {
+    public SkinCompatToolbar(@NonNull Context context) {
         this(context, null);
     }
 
-    public SkinCompatToolbar(Context context, @Nullable AttributeSet attrs) {
+    public SkinCompatToolbar(@NonNull Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, androidx.appcompat.R.attr.toolbarStyle);
     }
 
-    public SkinCompatToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public SkinCompatToolbar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mBackgroundTintHelper = new SkinCompatBackgroundHelper(this);
-        mBackgroundTintHelper.loadFromAttributes(attrs, defStyleAttr);
+        try {
+            mBackgroundTintHelper.loadFromAttributes(attrs, defStyleAttr);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to load background attributes", e);
+        }
 
         TypedArray a = context.obtainStyledAttributes(attrs, androidx.appcompat.R.styleable.Toolbar, defStyleAttr, 0);
         mNavigationIconResId = a.getResourceId(androidx.appcompat.R.styleable.Toolbar_navigationIcon, INVALID_ID);
@@ -66,35 +77,68 @@ public class SkinCompatToolbar extends Toolbar implements SkinCompatSupportable 
         applyNavigationIcon();
     }
 
+    /**
+     * 应用标题文本颜色。
+     */
     private void applyTitleTextColor() {
         mTitleTextColorResId = SkinCompatHelper.checkResourceId(mTitleTextColorResId);
         if (mTitleTextColorResId != INVALID_ID) {
-            setTitleTextColor(SkinCompatResources.getColor(getContext(), mTitleTextColorResId));
+            try {
+                setTitleTextColor(SkinCompatResources.getColor(getContext(), mTitleTextColorResId));
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to apply title text color: " + mTitleTextColorResId, e);
+            }
         }
     }
 
+    /**
+     * 应用副标题文本颜色。
+     */
     private void applySubtitleTextColor() {
         mSubtitleTextColorResId = SkinCompatHelper.checkResourceId(mSubtitleTextColorResId);
         if (mSubtitleTextColorResId != INVALID_ID) {
-            setSubtitleTextColor(SkinCompatResources.getColor(getContext(), mSubtitleTextColorResId));
+            try {
+                setSubtitleTextColor(SkinCompatResources.getColor(getContext(), mSubtitleTextColorResId));
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to apply subtitle text color: " + mSubtitleTextColorResId, e);
+            }
         }
     }
 
+    /**
+     * 应用导航图标。
+     */
     private void applyNavigationIcon() {
         mNavigationIconResId = SkinCompatHelper.checkResourceId(mNavigationIconResId);
         if (mNavigationIconResId != INVALID_ID) {
-            setNavigationIcon(SkinCompatVectorResources.getDrawableCompat(getContext(), mNavigationIconResId));
+            try {
+                setNavigationIcon(SkinCompatVectorResources.getDrawableCompat(getContext(), mNavigationIconResId));
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to apply navigation icon: " + mNavigationIconResId, e);
+            }
         }
     }
 
+    /**
+     * 设置背景资源，并通知换肤助手更新背景。
+     *
+     * @param resId 背景资源 ID
+     */
     @Override
     public void setBackgroundResource(@DrawableRes int resId) {
         super.setBackgroundResource(resId);
-        if (mBackgroundTintHelper != null) {
+        try {
             mBackgroundTintHelper.onSetBackgroundResource(resId);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set background resource: " + resId, e);
         }
     }
 
+    /**
+     * 设置导航图标，并更新换肤资源。
+     *
+     * @param resId 导航图标资源 ID
+     */
     @Override
     public void setNavigationIcon(@DrawableRes int resId) {
         super.setNavigationIcon(resId);
@@ -102,14 +146,18 @@ public class SkinCompatToolbar extends Toolbar implements SkinCompatSupportable 
         applyNavigationIcon();
     }
 
+    /**
+     * 应用当前皮肤，更新背景、标题颜色、副标题颜色和导航图标。
+     */
     @Override
     public void applySkin() {
-        if (mBackgroundTintHelper != null) {
+        try {
             mBackgroundTintHelper.applySkin();
+            applyTitleTextColor();
+            applySubtitleTextColor();
+            applyNavigationIcon();
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to apply skin", e);
         }
-        applyTitleTextColor();
-        applySubtitleTextColor();
-        applyNavigationIcon();
     }
-
 }
